4dff618aaa25740ec7b4d3941c895700cdb609d3
Caernarvon <zvirusmail@gmail.com>
2018-12-06 23:25:08 +0300

    algorithm optimized

--- src/main/java/sorters/AbstractBubbleSorter.java
+++ src/main/java/sorters/AbstractBubbleSorter.java
@@ -9,9 +9,24 @@ package sorters;
 abstract class AbstractBubbleSorter extends AbstractSorter {
 
     /**
-     * Abstract method, should be overrided by subtypes.
+     * Sort method. Uses {@code sortAlgorithm} to define sorting algorithm.
      *
-     * @param array array to sort
+     * @param array array to sort.
      */
-    public abstract void sort(int [] array);
+    public void sort(int [] array){
+        for (int i = 0; i < array.length; i++){
+            sortAlgorithm(array, i);
+        }
+    }
+
+    /**
+     * Abstract method, defines sorting algorithm, should be overrided by subtypes.
+     *
+     * @param array array to sort.
+     * @param i amount of elements in array, value reduces with each iteration to
+     *          to improve performance.
+     */
+    public abstract int[] sortAlgorithm(int [] array, int i);
+
+
 }
